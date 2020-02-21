20ec58f977ff7c061b27ec4aa09bda9cc99c6544
daffaviraka <daffanew99@gmail.com>
2020-02-21 11:19:24 +0700

    Intent Exercise

--- app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
@@ -3,12 +3,42 @@ package id.ac.polinema.intentexercise;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import id.ac.polinema.intentexercise.model.UserModel;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private TextView fullnameText;
+    private TextView emailText;
+    private TextView passwordText;
+    private TextView homepageText;
+    private TextView aboutText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        // TODO: bind here
+        fullnameText = findViewById(R.id.text_fullname);
+        emailText = findViewById(R.id.text_email);
+        passwordText = findViewById(R.id.text_password);
+        homepageText = findViewById(R.id.text_homepage);
+        aboutText = findViewById(R.id.text_about);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+
+            // TODO: display value here
+            UserModel user = extras.getParcelable(RegisterActivity.USER_KEY);
+
+            fullnameText.setText(user.getFullname());
+            emailText.setText(user.getEmail());
+            passwordText.setText(user.getPassword());
+            homepageText.setText(user.getHomepage());
+            aboutText.setText(user.getAbout());
+
+        }
     }
 }
--- app/src/main/java/id/ac/polinema/intentexercise/RegisterActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/RegisterActivity.java
@@ -2,13 +2,50 @@ package id.ac.polinema.intentexercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import id.ac.polinema.intentexercise.model.UserModel;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    public static String USER_KEY = "user";
+
+    private TextInputEditText fullnameInput;
+    private TextInputEditText emailInput;
+    private TextInputEditText passwordInput;
+    private TextInputEditText homepageInput;
+    private TextInputEditText aboutInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        //bind
+        fullnameInput = findViewById(R.id.text_fullname);
+        emailInput = findViewById(R.id.text_email);
+        passwordInput = findViewById(R.id.text_password);
+        homepageInput = findViewById(R.id.text_homepage);
+        aboutInput = findViewById(R.id.text_about);
+
+    }
+
+    public void handleSubmit(View view){
+        String fullname = fullnameInput.getText().toString();
+        String email = emailInput.getText().toString();
+        String password = passwordInput.getText().toString();
+        String homepage = homepageInput.getText().toString();
+        String about = aboutInput.getText().toString();
+
+        Intent intent = new Intent(this,ProfileActivity.class);
+
+        intent.putExtra(USER_KEY, new UserModel(fullname,email,password,homepage,about));
+
+        startActivity(intent);
+
     }
 }
